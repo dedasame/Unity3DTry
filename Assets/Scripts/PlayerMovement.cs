7d9bbcef5fe9d0c2726ea52b7ff9bc6897ff010b
dedasame <ecekarakoc36@gmail.com>
2024-01-14 16:48:58 +0300

    My Commit
    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck; //Player'ýn altýndaki object-> yerle temas ediyorsa zýplamaya izin vermek için
+    [SerializeField] LayerMask ground;
+
+    void Start() //1 kere çalýþýr start da
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    
+    void Update() //works 60 times for 1 sec.
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x,jumpForce, rb.velocity.z);
+        }
+
+    }
+
+    bool IsGrounded()
+    {
+
+        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
+
+
+    }
+
+
+
+}
